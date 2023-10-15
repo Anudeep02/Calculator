479f5ddecfa1ba7239e3fb12b4e93384706d625c
Anudeep02 <74246826+Anudeep02@users.noreply.github.com>
2023-10-15 11:32:53 +0530

    Add files via upload

---
+++ linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,15 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/anudeep/snap/flutter/common/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/anudeep/fluttertraining/calculator" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/anudeep/snap/flutter/common/flutter"
+  "PROJECT_DIR=/home/anudeep/fluttertraining/calculator"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/anudeep/fluttertraining/calculator/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/anudeep/fluttertraining/calculator/lib/main.dart"
+)
